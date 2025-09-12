0c732f3e4975e5bfb326f2f4f9108c7f631a17a3
ManikantaVeduruparthi <sivamanikanta514@gmail.com>
2025-09-12 14:52:32 +0530

    updated successfully

--- toggle.js
+++ toggle.js
@@ -1,7 +1,7 @@
 const toggle = document.getElementById('bgToggle');
 toggle.addEventListener('change', () => {
     if(toggle.checked){
-        document.body.style.background = 'linear-gradient(145deg, #00A0DF, #FF0000, #6E2CA5)';
+        document.body.style.background = 'linear-gradient(145deg, #ff6600)';
     } else {
         document.body.style.background = 'black';
     }
